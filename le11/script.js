7e4561a2d29d47469b4bf7bc4fbc00977fb1b226
M-elsawy <mohamedelsawy5436@gmail.com>
2023-10-29 23:09:11 +0200

    le-11

---
+++ le11/script.js
@@ -0,0 +1,68 @@
+// document.write("<h1>some text</h1>")
+
+
+// var element=document.getElementsByTagName("div")[0] 
+// element.innerHTML="java script"
+// element.style.backgroundColor="red"
+// element.style.color="#fff"
+
+
+// var text = "Lorem ipsum dolor sit amet consectetur adipisicing"+
+//  "elit. Ipsa unde nihil dolorem aliquam consectetur optio "+
+//  "accusamus. Illum nulla saepe qui, repudiandae modi minus"+
+//   "debitis sunt aspernatur velit. Animi, odit saepe"
+// console.log(text);
+
+// var text2 ="Lorem ipsum dolor sit amet consectetur adipisicing \
+//  elit. Ipsa unde nihil dolorem aliquam consectetur optio \
+//  accusamus. Illum nulla saepe qui, repudiandae modi minus\
+//   debitis sunt aspernatur velit. Animi, odit saepe?"
+//   console.log(text2);
+//   var text3 = `Lorem ipsum dolor sit amet consectetur adipisicing
+//    elit. Ipsa unde nihil dolorem aliquam consectetur optio accusamus.
+//     Illum nulla saepe qui, repudiandae modi minus debitis 
+//     sunt aspernatur velit. Animi, odit saepe?
+//     `
+//     console.log(text3);
+
+
+// var le = 11
+
+
+// var text = `welcome le ${le} java script`
+
+// console.log(text);
+
+
+
+var text = "some text"
+var box = `
+<div class="box">
+        <h1>${text}</h1>
+    </div>
+`
+document.write(box)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
