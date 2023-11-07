0201483f0d7a98b82d9d74889ae3fbeda1c67247
M-elsawy <mohamedelsawy5436@gmail.com>
2023-11-07 16:16:53 +0200

    le-12

---
+++ le12/script1.js
@@ -0,0 +1,224 @@
+// var str = "welcome java script"
+// console.log(str);
+// console.log(str.length);
+// console.log(str.charAt(6));
+// console.log(str.indexOf("e"));
+// console.log(str.startsWith("welcome"));
+// console.log(str.endsWith("t"))
+// console.log(str.includes("ahmed"));
+// console.log(str.replace("java","JAVA"));
+
+// var newStr = str.replace("java","JAVA2")
+// console.log(newStr);
+// console.log(str.toLocaleLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.slice(7));
+// console.log(str.slice(0,7));
+// console.log(str.slice(-6));
+
+// var str2 = "ahmed,sara,mohamed,tamer"
+// console.log(str2);
+// console.log(str2.split(","));
+
+// var str3 = str2.split(",")
+
+// console.log(str3);
+
+
+// var user_name="ahmed"
+// if(user_name=="mohamed"){
+// console.log("welcome admin");
+// }else{
+//     console.log("welcome user");
+// }
+
+// if(user_name.startsWith("a")){
+//     console.log("starts With A");
+// }else{
+//     console.log("starts With !=A ");
+// }
+
+// var user_name="ahmed"
+// if(user_name != "ahmed"){
+// console.log("welcome user");
+// }else{
+//     console.log("welcome admin");
+// }
+
+
+// var item = "item1"
+// var price = 100
+// if(item=="item1"&&price==100){
+//     document.write(`item : ${item}<br>  price: ${price}`)
+// }else{
+//     console.log("......");
+// }
+
+// var first_name = "mohamed"
+// var last_name = "ahmed"
+// if(first_name=="mohamed" || last_name=="elsawy"){
+//     console.log("welcome admin");
+// }else{
+//     console.log("welcome user");
+// }
+
+
+// var user_name = "sara"
+// if(user_name=="mohamed"){
+//     console.log(`welcome admin ${user_name}`);
+// }else if(user_name=="tamer"){
+//  console.log(`welcome admin ${user_name}`);
+// }else{
+//     console.log("welcome user ");
+// }
+// var x = 100
+// if(x==10){
+//     console.log("x=10");
+//     var y = 20
+//     if(y==20){
+//         console.log("y=20");
+//     }else{
+//         console.log("y !=20");
+//     }
+// }else{
+//     console.log("x !=10");
+// }
+
+// for(var i = 0 ; i<10 ; i++){
+// document.write(`<h3>${i} welcome</h3>`)
+// }
+
+
+// debugger
+// for(var i = 0 ; i<5 ; i++){
+//     document.write(`<h3>${i} welcome</h3>`)
+// }
+
+// var i = 10
+// while(i<5){
+//     document.write(`<h3>${i} welcome</h3>`)
+//     i++
+// }
+
+// var i = 10
+// do{
+//     document.write(`<h3>${i} welcome</h3>`)
+//     i++
+// }while(i<5)
+
+// var arr = ["ahmed","tamer","sara"]
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i] +"<br>")
+// }
+// var arr = ["ahmed","tamer","sara"]
+
+// for(var el of arr){
+//     document.write(el+"<br>")
+// }
+
+// var str = prompt("enter name")
+
+// console.log(str);
+// var x = confirm()
+// console.log(x);
+
+
+document.getElementsByTagName("h1")[0].innerHTML="welcome"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
